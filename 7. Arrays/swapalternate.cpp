20dd75a05bdb0adb3c676ef204705e106271bf53
CyberBoyAyush <aayush240204@gmail.com>
2024-02-01 23:58:45 +0530

    commit1

---
+++ 7. Arrays/swapalternate.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void printArray(int arr[],int size){
+    for(int i =0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void swapAlternate(int arr[], int size){
+    for(int i = 0; i <size; i+=2){
+        if(i+1<size){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+}
+
+int main(){
+    int even[6] = {2,3,4,5,6,0};
+    int odd[5] = {2,3,5,6,0};
+
+    swapAlternate(even,6);
+    swapAlternate(odd,5);
+
+    printArray(even,6);
+    printArray(odd,5);
+}
