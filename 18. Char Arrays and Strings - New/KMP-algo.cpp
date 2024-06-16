ff8d8214b6765e188fc56b56f0e436dabb3d68a9
CyberBoyAyush <aayush240204@gmail.com>
2024-06-16 18:40:19 +0530

    KMP ALGO

--- 18. Char Arrays and Strings - New/KMP-algo.cpp
+++ 18. Char Arrays and Strings - New/KMP-algo.cpp
@@ -1,9 +1,6 @@
-//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
-
-// } Driver Code Ends
 class Solution
 { 
     public: 
@@ -67,24 +64,3 @@ class Solution
         }
      
 };
-
-//{ Driver Code Starts.
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        string S, pat;
-        cin >> S >> pat;
-        Solution ob;
-        vector <int> res = ob.search(pat, S);
-        if (res.size()==0)
-            cout<<-1<<endl;
-        else {
-            for (int i : res) cout << i << " ";
-            cout << endl;
-        }
-    }
-    return 0;
-}
