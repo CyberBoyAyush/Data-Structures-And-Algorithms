a8eec17291a5f7f3860cad61a40a5852cb3b8461
CyberBoyAyush <aayush240204@gmail.com>
2024-06-18 18:17:21 +0530

    action 3

--- index_files.py
+++ index_files.py
@@ -1,15 +1,35 @@
 import os
 
 def generate_file_index(root_dir):
-    file_list = []
+    file_tree = {}
     for dirpath, _, filenames in os.walk(root_dir):
+        rel_dir = os.path.relpath(dirpath, root_dir)
+        if rel_dir == ".":
+            rel_dir = ""
         for filename in filenames:
             if filename != 'README.md':  # Exclude the README itself
-                filepath = os.path.relpath(os.path.join(dirpath, filename), root_dir)
-                file_list.append(filepath)
-    return file_list
+                file_path = os.path.join(rel_dir, filename)
+                dirs = rel_dir.split(os.sep) if rel_dir else []
+                node = file_tree
+                for d in dirs:
+                    if d not in node:
+                        node[d] = {}
+                    node = node[d]
+                node[filename] = file_path
+    return file_tree
 
-def update_readme(file_list, readme_path='README.md'):
+def render_file_tree(file_tree, base_path=""):
+    md_lines = []
+    for key, value in sorted(file_tree.items()):
+        if isinstance(value, dict):
+            md_lines.append(f'<details><summary>{key}</summary>')
+            md_lines.append(render_file_tree(value, base_path + key + '/'))
+            md_lines.append('</details>')
+        else:
+            md_lines.append(f'- [{key}]({base_path + key})')
+    return '\n'.join(md_lines)
+
+def update_readme(file_tree, readme_path='README.md'):
     with open(readme_path, 'r') as file:
         lines = file.readlines()
 
@@ -20,12 +40,13 @@ def update_readme(file_list, readme_path='README.md'):
     end_index = next((i for i, line in enumerate(lines) if end_marker in line), None)
 
     if start_index is not None and end_index is not None:
-        lines = lines[:start_index + 1] + [f'- {file}\n' for file in file_list] + lines[end_index:]
+        file_tree_md = render_file_tree(file_tree)
+        lines = lines[:start_index + 1] + [file_tree_md + '\n'] + lines[end_index:]
 
         with open(readme_path, 'w') as file:
             file.writelines(lines)
 
 if __name__ == "__main__":
     root_directory = '.'
-    files = generate_file_index(root_directory)
-    update_readme(files)
+    file_tree = generate_file_index(root_directory)
+    update_readme(file_tree)
